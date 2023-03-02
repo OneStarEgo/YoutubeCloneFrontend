db96d6e3ca53e84d84acbf1dbd9c4ea6452ab2af
Rodney McGee <rodneymcgee73@gmail.com>
2023-03-02 12:34:03 -0600

    fix: Updating Production URL_HOST

--- src/urlHost.js
+++ src/urlHost.js
@@ -1,4 +1,4 @@
 export const URL_HOST =
   process.env.NODE_ENV === "production"
-    ? "PRODUCTION BASE URL HERE"
+    ? "https://cors-everywhere-me.herokuapp.com/http://youtubeclonebackend-env.eba-fh5jh6y5.us-east-2.elasticbeanstalk.com"
     : "http://127.0.0.1:8000";
